fdba80761f062fbaa680f684c84d0914902c219a
petrakalulu <petrakalulu001@gmail.com>
2023-04-24 05:40:26 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 ld = n % 10;
 if (ld > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n", n, ld);
+printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 }
 else if (ld == 0)
 {
-printf("last digit of %d is %d and is \n0", n, ld);
+printf("Last digit of %d is %d and is \n0", n, ld);
 }
 else
 {
-printf("last digit of %d is %d and less than 6 and not 0\n", n, ld);
+printf("Last digit of %d is %d and less than 6 and not 0\n", n, ld);
 }
 return (0);
 }
