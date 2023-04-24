bd28a4a6f5b19e277a63a6343b5ce10c40e39d00
petrakalulu <petrakalulu001@gmail.com>
2023-04-24 06:36:00 -0700

    Tas 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'prints the alphabet in lowercase and then in uppercase followed by a new line'
+ * Description: 'prints the alphabet in lowercase and then in uppercase
+ * followed by a new line'
  * Return: Always 0 (Success)
  */
 int main(void)
