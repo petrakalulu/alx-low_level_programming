7c7cd56d3d669136d928ef05800325d4f82ecd2d
petrakalulu <petrakalulu001@gmail.com>
2023-04-25 10:19:02 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,15 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * add - adds two integers and returns the result
  * @a: first integer to be added
  * @b: second integer to be added
- * Return: sum of a and b
+ * Return: sum of the two integers
  */
 int add(int a, int b)
 {
-int c;
-c = a + b;
-return (c);
+int sum = a + b;
+printf("The sum of %d and %d is %d\n", a, b, sum);
+return (sum);
 }
+
--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,20 +1,13 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
-/* Function prototype */
 int add(int a, int b);
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    int n;
+int main(void) {
+   int n;
 
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
+   n = add(89, 9);
+   printf("%d\n", n);
+   return 0;
 }
 
