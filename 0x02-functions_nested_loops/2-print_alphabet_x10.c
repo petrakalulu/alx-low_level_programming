f916d062a055d4100d52bce909e4010496d94d74
petrakalulu <petrakalulu001@gmail.com>
2023-04-25 05:32:35 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * main - Entry point
- * Description: 'prints 10 times the alphabet in lowercase'
- * Return: Always 0 (Success)
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
  */
 void print_alphabet_x10(void)
 {
