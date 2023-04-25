2ddd61cfc26bce6ede69c7b61af74aa21e5c9b09
petrakalulu <petrakalulu001@gmail.com>
2023-04-25 06:18:50 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  * Return: 1 is greater than 0. 0 is zero
  * -1 is less than zero
  */
-int print_sign(int n);
+int print_sign(int n)
 {
 if (n > 0)
 {
@@ -17,7 +17,7 @@ else if (n < 0)
 _putchar(45);
 return (-1);
 }
-else 
+else
 {
 _putchar(48);
 return (0);
