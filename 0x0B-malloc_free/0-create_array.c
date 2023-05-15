64e0ca5c0a010df835a9c15209f5c250072a70d2
petrakalulu <petrakalulu001@gmail.com>
2023-05-15 11:39:53 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NUL)
+	if (size == 0 || str == NULL)
 
 		return (NULL);
-	for (i = 0; i < sizee; i++)
+	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
-`}
+}
