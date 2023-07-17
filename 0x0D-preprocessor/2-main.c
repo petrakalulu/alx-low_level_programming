a464485806d618c5fbb1767a067eac34fafda4f1
petrakalulu <petrakalulu001@gmail.com>
2023-07-17 14:02:52 -0700

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
- */
-int ain(void)
+* main - prints the name of the file
+* Description: program that prints the name of a file
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
+
